c1a46581fa5911a87fdfbb9323d0428bc6a3ab09
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2023-11-20 14:30:49 -0500

    [Add] Files to project & [Update] Previous files

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log('Javascript Conectado');
+const userEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+
+userEmail.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+  desktopMenu.classList.toggle('inactive');
+}
